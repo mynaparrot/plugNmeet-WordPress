36042ac8d0256bca21ff96fe2785dbb8f89c9371
jibon Costa <jiboncosta57@gmail.com>
2022-07-24 21:21:23 +0600

    fix: table wasn't change

--- plugnmeet/includes/class-plugnmeet-on-after-update.php
+++ plugnmeet/includes/class-plugnmeet-on-after-update.php
@@ -13,7 +13,7 @@ class PlugNmeetOnAfterUpdate {
     private function databaseTableUpdate() {
         global $wpdb;
 
-        if (version_compare(PLUGNMEET_VERSION, '1.0.10', '<')) {
+        if (version_compare(PLUGNMEET_VERSION, '1.0.11', '=')) {
             $column = $wpdb->get_results($wpdb->prepare(
                 "SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = %s AND TABLE_NAME = %s AND COLUMN_NAME = %s ",
                 DB_NAME, $wpdb->prefix . 'plugnmeet_rooms', 'roles'
