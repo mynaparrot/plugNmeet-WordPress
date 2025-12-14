c0646327edf3e29675dd810fc9965ac9f5b03a35
Jibon L. Costa <jiboncosta57@gmail.com>
2025-12-14 16:23:43 +0600

    fix

--- plugnmeet/helpers/libs/plugnmeet-sdk-php/vendor/ramsey/collection/src/Exception/InvalidPropertyOrMethod.php
+++ plugnmeet/helpers/libs/plugnmeet-sdk-php/vendor/ramsey/collection/src/Exception/InvalidPropertyOrMethod.php
@@ -17,8 +17,10 @@ namespace Ramsey\Collection\Exception;
 use RuntimeException;
 
 /**
- * Thrown when attempting to use a sort order that is not recognized.
+ * Thrown when attempting to evaluate a property, method, or array key
+ * that doesn't exist on an element or cannot otherwise be evaluated in the
+ * current context.
  */
-class InvalidSortOrderException extends RuntimeException
+class InvalidPropertyOrMethod extends RuntimeException implements CollectionException
 {
 }
