cef7092dc3be91919ac4eb8e466b913e5adfcfb0
Jibon L. Costa <jiboncosta57@gmail.com>
2026-06-05 21:40:22 +0200

    fix: replaced `<p>` tags with `<div>`

--- plugnmeet/public/partials/parts/login-form.php
+++ plugnmeet/public/partials/parts/login-form.php
@@ -20,28 +20,27 @@ $currentUrl = home_url( add_query_arg( null, null ) );
 <form class="plugnmeet-login-form" data-room-id="<?php echo esc_attr( $roomInfo->room_id ); ?>">
     <div class="notice notice-info roomStatus" role="alert" style="display: none"></div>
 
-    <p class="form-row">
+    <div class="form-row">
         <label for="name"><?php echo __( "Name", "plugnmeet" ) ?></label>
         <input type="text" name="name" id="name" required
                value="<?php echo esc_attr( $user->display_name ) ?>"
                placeholder="<?php echo __( "Your full name", "plugnmeet" ) ?>"
         >
-    </p>
+    </div>
 
     <?php if ( isset( $role['require_password'] ) && $role['require_password'] === "on" ): ?>
-        <p class="form-row">
+        <div class="form-row">
             <label for="room-password"><?php echo __( "Password", "plugnmeet" ) ?></label>
             <input type="password" name="password" id="room-password" required
                    placeholder="<?php echo __( "Room's Password", "plugnmeet" ) ?>"
             >
-        </p>
+        </div>
     <?php endif; ?>
 
     <input type="hidden" name="id" value="<?php echo esc_attr( $roomInfo->id ) ?>">
     <input type="hidden" name="action" value="plugnmeet_login_to_room">
     <input type="hidden" name="current_url" value="<?php echo esc_attr( urlencode( $currentUrl ) ) ?>">
-    <input type="hidden" name="nonce"
-           value="<?php echo wp_create_nonce( 'plugnmeet_login_to_room' ) ?>">
+    <input type="hidden" name="nonce" value="<?php echo wp_create_nonce( 'plugnmeet_login_to_room' ) ?>">
 
     <div class="form-submit">
         <button type="submit" class="button button-primary"><?php echo __( "Join", "plugnmeet" ) ?></button>
